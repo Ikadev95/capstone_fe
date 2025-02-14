cc363c1a5e451aeea4745cde59ea2668eadebf42
Ikadev95 <fegughi@gmail.com>
2025-02-14 18:02:32 +0100

    home work in progress

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: false,
-  
+
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  constructor(private http: HttpClient) {}
+
+  downloadPDF() {
+    const link = document.createElement('a');
+    link.href = 'bando.pdf'; // Percorso del file nel frontend
+    link.download = 'bando.pdf'; // Nome del file scaricato
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 
 }
